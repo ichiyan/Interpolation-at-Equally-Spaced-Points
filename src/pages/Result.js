45cf26423e676197c22f913168d800db67c8d1f3
Riasnner <73977565+Riasnner@users.noreply.github.com>
2023-05-20 21:30:15 +0800

    Update Result.js

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -7,6 +7,14 @@ const Result = ({data, method}) => {
   var points = [];
   var column_labels = [];
 
+  const dummy = [
+    {x: 0.0, y: 0.000, dif1: 1.015, dif2: 0.2124999999999999, dif3: 0.5000000000000032, dif4:0.5208333333333183},
+    {x: 0.2, y: 0.203, dif1: 1.0999999999999999, dif2: 0.5125000000000018, dif3: 0.9166666666666579	},
+    {x: 0.4, y: 0.423, dif1: 1.3050000000000006, dif2: 1.0624999999999967},
+    {x: 0.6, y: 0.684, dif1: 1.7299999999999993},
+    {x: 0.8, y: 1.030},
+  ];
+
   // calculator with input function
   if (method == 1){
       console.log(data)
@@ -86,17 +94,21 @@ const Result = ({data, method}) => {
             </tr>
           </thead>
           <tbody>
-              {points.map((point, ndx) => {
+              {dummy.map((point, ndx) => {
                 return (
                   <tr key={ndx}>
                     <td>{point.x}</td>
                     <td>{point.y}</td>
-                        {
+                    <td>{point.dif1}</td>
+                    <td>{point.dif2}</td>
+                    <td>{point.dif3}</td>
+                    <td>{point.dif4}</td>
+                        {/* {
                           column_labels.map((col, ndx) => {
-                            return(<td key={ndx}  scope="col">
+                            return(<td key={ndx}  scope="col">0.00123
                             </td>)
                           })
-                        }
+                        } */}
                   </tr>
                 )
               })}
