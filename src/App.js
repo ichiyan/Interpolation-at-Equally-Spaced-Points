b51dbaf3b6e691e171b1fe0f5c0442c4b61e3b7d
CoffeeDrinkingProgrammer <paulo.ata88@gmail.com>
2023-05-20 18:16:33 +0800

    calculator draft

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputForm from "./form/form";
+import InputForm2 from "./form/form3";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {/*<InputForm></InputForm>*/}
+          <InputForm2></InputForm2>
       </header>
     </div>
   );
