d3c822f3ae01f1c63471c436b144f0dfcebac9e6
ichiyan <gabriela.balisacan01@gmail.com>
2023-05-21 15:48:21 +0800

    -

--- src/pages/Conclusion.js
+++ src/pages/Conclusion.js
@@ -4,6 +4,13 @@ const Conclusion = ({}, ref) => {
     return(
         <div className="container pt-3">
             <h2 ref={ref}>Conclusion</h2>
+            <br></br>
+            <p>
+                When the x values are equally spaced, an interpolating polynomial, a function
+                that passes through all the given points with no regard to the points between them,
+                can be successfully derived using the Newton-Gregory forward polynomial approach, as
+                demonstrated in this online calculator.
+            </p>
         </div>
     )
 }
--- src/pages/Specification.js
+++ src/pages/Specification.js
@@ -1,4 +1,5 @@
 import {React, forwardRef} from "react";
+import Equation from "./Equation";
 
 const Specification = ({}, ref) => {
     return(
@@ -6,10 +7,12 @@ const Specification = ({}, ref) => {
             <h2 ref={ref}>Specifications</h2>
             <br></br>
             <ul className="specifications">
-                <li>Inputs for initial value of x, number of x values, difference between x values, and y values can be any rational number (not in fraction form).</li>
-                <li>Inputs for the expression are valid so long as they are mathematical, the unknown variable is x and only x, and they follow the <a href="https://mathjs.org/docs/expressions/syntax.html" target="_blank">Math JS expression syntax</a>.
-                </li>
-                <li>Accuracy: The computation is done with Number type variables, which is a floating point number. It has a limited precision of 64 bits [approx. 16 digits]. Values that exceed this limit will suffer from round-off errors during calculation.</li>
+                <li>The calculator outputs an interpolating polynomial only of degree <Equation math="n"/> where <Equation math="n"/> is one less than the number of points. </li>
+                <li>The initial value of x, the difference between x values, and the y values must be a rational number (not in fraction form).</li>
+                <li>The number of x values must be a positive integer.</li>
+                <li>Inputs for the expression are valid so long as they are mathematical, the unknown variable is x and only x, and they follow the <a href="https://mathjs.org/docs/expressions/syntax.html" target="_blank">Math JS expression syntax</a>.</li>
+                <li>The computations are done using floating point numbers with a limited precision of 64 bits (approx. 16 decimal digits). Values that exceed this limit will suffer from round-off errors during calculation.</li>
+                <li>The interpolating polynomial may not be in its simplest form.</li>
             </ul>
         </div>
     )
